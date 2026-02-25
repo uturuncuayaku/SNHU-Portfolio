3ec08b2a955dbc13095ddd7ed38e55bad90baab0
Andres <57707223+uturuncuayaku@users.noreply.github.com>
2026-02-25 11:24:31 -0600

    Add files via upload

---
+++ CS 405 Secure Coding/Code/SQL Injection/SQLInjection.cpp
@@ -0,0 +1,273 @@
+// SQLInjection.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <algorithm>
+#include <iostream>
+#include <locale>
+#include <tuple>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <cctype>
+#include "sqlite3.h"
+
+// DO NOT CHANGE
+typedef std::tuple<std::string, std::string, std::string> user_record;
+const std::string str_where = " where ";
+
+// DO NOT CHANGE
+static int callback(void* possible_vector, int argc, char** argv, char** azColName)
+{
+  if (possible_vector == NULL)
+  { // no vector passed in, so just display the results
+    for (int i = 0; i < argc; i++) 
+    {
+      std::cout << azColName[i] << " = " << (argv[i] ? argv[i] : "NULL") << std::endl;
+    }
+    std::cout << std::endl;
+  }
+  else
+  {
+    std::vector< user_record >* rows =
+      static_cast<std::vector< user_record > *>(possible_vector);
+
+    rows->push_back(std::make_tuple(argv[0], argv[1], argv[2]));
+  }
+  return 0;
+}
+
+// DO NOT CHANGE
+bool initialize_database(sqlite3* db)
+{
+  char* error_message = NULL;
+  std::string sql = "CREATE TABLE USERS(" \
+    "ID INT PRIMARY KEY     NOT NULL," \
+    "NAME           TEXT    NOT NULL," \
+    "PASSWORD       TEXT    NOT NULL);";
+
+  int result = sqlite3_exec(db, sql.c_str(), callback, NULL, &error_message);
+  if (result != SQLITE_OK)
+  {
+    std::cout << "Failed to create USERS table. ERROR = " << error_message << std::endl;
+    sqlite3_free(error_message);
+    return false;
+  }
+  std::cout << "USERS table created." << std::endl;
+
+  // insert some dummy data
+  sql = "INSERT INTO USERS (ID, NAME, PASSWORD)" \
+    "VALUES (1, 'Fred', 'Flinstone');" \
+    "INSERT INTO USERS (ID, NAME, PASSWORD)" \
+    "VALUES (2, 'Barney', 'Rubble');" \
+    "INSERT INTO USERS (ID, NAME, PASSWORD)" \
+    "VALUES (3, 'Wilma', 'Flinstone');" \
+    "INSERT INTO USERS (ID, NAME, PASSWORD)" \
+    "VALUES (4, 'Betty', 'Rubble');";
+
+  result = sqlite3_exec(db, sql.c_str(), callback, NULL, &error_message);
+  if (result != SQLITE_OK)
+  {
+    std::cout << "Data failed to insert to USERS table. ERROR = " << error_message << std::endl;
+    sqlite3_free(error_message);
+    return false;
+  }
+
+  return true;
+}
+//********************************************************
+bool checkForInjection(std::string in, int currentIndex){
+  std::string checkString;
+  int len = in.size();
+  for (int i = 0; i < len-currentIndex; ++i){
+    checkString.push_back(in[i+currentIndex-1]);
+  }
+  //find the equals sign
+  int pos = checkString.find("=");
+  std::string ans;
+  for (int i = 1; i < pos; ++i) {
+    ans.push_back(checkString[i]);
+  }
+  std::string ans2;
+  int k = pos+1;
+  for (int i = len-currentIndex; i > pos; --i){
+    ans2.push_back(checkString[i]);
+  }
+
+  if (ans.compare(ans2)){
+    std::cout << "\tError!";
+    return true;
+  }
+  std::cout << std::endl;
+  return false;
+}
+bool parseQuery(const std::string& in) {
+  std::string input = "";
+  int n = 0;
+  // creates a new lower case string from user input
+  for (char chr: in) {
+    if (std::isupper(chr)) {
+      input.push_back(std::tolower(chr));
+    }else {
+      input.push_back(std::tolower(chr));
+    }
+    n++;
+    // Check to see when we find the keyword 'or'
+    if (n >= 4 && 
+        input[n-2] == 'r' && 
+        input[n-3] == 'o' && 
+        input[n-4] != 'w') {
+      // Found the keyword 'or'
+      if (checkForInjection(in, n)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+//*************************************************************
+bool run_query(sqlite3* db, const std::string& sql, std::vector< user_record >& records)
+{
+
+  // clear any prior results
+  records.clear();
+
+  // If the query doesn't have a weird 'or' condition
+  // that evaluates to true, we have parsed it correctly
+  // otherwise we don't let the query run on the database
+  if(parseQuery(sql)) {
+    std::cout << " Please review sql query for possible injection!" << std::endl;
+    return false;
+  }
+
+  char* error_message;
+  if(sqlite3_exec(db, sql.c_str(), callback, &records, &error_message) != SQLITE_OK)
+  {
+    std::cout << "Data failed to be queried from USERS table. ERROR = " << error_message << std::endl;
+    sqlite3_free(error_message);
+    return false;
+  }
+
+  return true;
+}
+
+// DO NOT CHANGE
+bool run_query_injection(sqlite3* db, const std::string& sql, std::vector< user_record >& records)
+{
+  std::string injectedSQL(sql);
+  std::string localCopy(sql);
+
+  // we work on the local copy because of the const
+  std::transform(localCopy.begin(), localCopy.end(), localCopy.begin(), ::tolower);
+  if(localCopy.find_last_of(str_where) >= 0)
+  { // this sql has a where clause
+    if(localCopy.back() == ';')
+    { // smart SQL developer terminated with a semicolon - we can fix that!
+      injectedSQL.pop_back();
+    }
+
+    switch (rand() % 4)
+    {
+    case 1:
+      injectedSQL.append(" or 2=2;");
+      break;
+    case 2:
+      injectedSQL.append(" or 'hi'='hi';");
+      break;
+    case 3:
+      injectedSQL.append(" or 'hack'='hack';");
+      break;
+    case 0:
+    default:
+      injectedSQL.append(" or 1=1;");
+      break;
+    }
+  }
+  
+  return run_query(db, injectedSQL, records);
+}
+
+
+// DO NOT CHANGE
+void dump_results(const std::string& sql, const std::vector< user_record >& records)
+{
+  std::cout << std::endl << "SQL: " << sql << " ==> " << records.size() << " records found." << std::endl;
+
+  for (auto record : records)
+  {
+    std::cout << "User: " << std::get<1>(record) << " [UID=" << std::get<0>(record) << " PWD=" << std::get<2>(record) << "]" << std::endl;
+  }
+}
+
+// DO NOT CHANGE
+void run_queries(sqlite3* db)
+{
+  char* error_message = NULL;
+
+  std::vector< user_record > records;
+
+  // query all
+  std::string sql = "SELECT * from USERS";
+  if (!run_query(db, sql, records)) return;
+  dump_results(sql, records);
+
+  //  query 1
+  sql = "SELECT ID, NAME, PASSWORD FROM USERS WHERE NAME='Fred'";
+  if (!run_query(db, sql, records)) return;
+  dump_results(sql, records);
+
+  //  run query 1 with injection 5 times
+  for (auto i = 0; i < 5; ++i)
+  {
+    if (!run_query_injection(db, sql, records)) continue;
+    dump_results(sql, records);
+  }
+
+}
+
+// You can change main by adding stuff to it, but all of the existing code must remain, and be in the
+// in the order called, and with none of this existing code placed into conditional statements
+int main()
+{
+  // initialize random seed:
+  srand(time(nullptr));
+
+  int return_code = 0;
+  std::cout << "SQL Injection Example" << std::endl;
+
+  // the database handle
+  sqlite3* db = NULL;
+  char* error_message = NULL;
+
+  // open the database connection
+  int result = sqlite3_open(":memory:", &db);
+
+  if(result != SQLITE_OK)
+  {
+    std::cout << "Failed to connect to the database and terminating. ERROR=" << sqlite3_errmsg(db) << std::endl;
+    return -1;
+  }
+
+  std::cout << "Connected to the database." << std::endl;
+
+  // initialize our database
+  if(!initialize_database(db))
+  {
+    std::cout << "Database Initialization Failed. Terminating." << std::endl;
+    return_code = -1;
+  }
+  else
+  {
+    run_queries(db);
+  }
+
+  // close the connection if opened
+  if(db != NULL)
+  {
+    sqlite3_close(db);
+  }
+
+  return return_code;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
